ead9ceb76b719455e57c8ce6be299363192436ae
LyraT57 <54109816+LyraT57@users.noreply.github.com>
2020-07-17 22:16:57 +0800

    numb

---
+++ pee_hey_send_cheese.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+	# -*- coding: UTF-8 -*-
+import random
+text = [ 
+	"喵喵 ",
+	"喵 ",
+	"喵！ ",
+	"喵喵喵？",
+	"要抱 ",
+	"抱 ",
+        "抱住",
+	"咕咕咕 ",
+	"嘤嘤嘤",
+	"揉揉揉 ",
+	"揉……",
+	"蹭蹭"
+        "QAQ",
+        "qwq",
+	]
+xx=text[random.randint(0,len(text)-1)]
+print(xx)
+a=input(" ")
